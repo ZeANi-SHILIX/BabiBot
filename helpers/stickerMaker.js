72d00e76a85ba0ec9e9453dc2c9a341fd12e5e4b
sHiLo BaBilA <shilobabila1@gmail.com>
2023-06-25 22:48:30 +0300

    Update stickerMaker.js

--- helpers/stickerMaker.js
+++ helpers/stickerMaker.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { downloadMediaMessage } from '@adiwajshing/baileys';
 import { Sticker, StickerTypes } from 'wa-sticker-formatter';
 import ffmpegInstaller from '@ffmpeg-installer/ffmpeg';
