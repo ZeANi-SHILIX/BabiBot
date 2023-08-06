a1dc23f9cc9dfebe23e30768258a5fd36e96ddc6
Shilo Babila <shilobabila1@gmail.com>
2023-08-07 00:47:31 +0300

    Update berkuniHandler.js

--- helpers/berkuniHandler.js
+++ helpers/berkuniHandler.js
@@ -1,18 +1,16 @@
-
 import MemoryStore from '../src/store.js'
 import { downloadMediaMessage } from '@adiwajshing/baileys';
 import barkuniDB from '../src/barkuni.js';
 import { Sticker } from 'wa-sticker-formatter';
-import messageRetryHandler from "../src/retryHandler.js";
+import { sendCustomMsgQueue, errorMsgQueue } from '../src/QueueObj.js';
 
 
 /**
  * when ```!barkuni``` is called, this function will be called
  * @param {import('@adiwajshing/baileys').proto.WebMessageInfo} msg 
- * @param {import('@adiwajshing/baileys').WASocket} sock 
  * @param {String} superuser
  */
-async function BarkuniSticker(msg, sock, superuser) {
+async function BarkuniSticker(msg, superuser) {
     let id = msg.key.remoteJid;
 
     // save sticker to database
@@ -22,55 +20,55 @@ async function BarkuniSticker(msg, sock, superuser) {
 
         let quotedMessage = await MemoryStore.loadMessage(id, msgID);
         // if no quoted message, continue to search for sticker
-        if (!quotedMessage) return searchBarkuni(sock, id);
+        if (!quotedMessage) return searchBarkuni(id);
 
         // check if sender is superuser
         if (!(id?.includes(superuser) || msg.key.participant?.includes(superuser)))
-            return sock.sendMessage(id, { text: "אופס... אין לך הרשאה להוסיף סטיקרי ברקוני" });
-
+            return searchBarkuni(id);
 
         try {
             // download sticker
             let stickerBuffer = await downloadMediaMessage(quotedMessage, 'buffer', {});
             barkuniDB.exists({ buffer: stickerBuffer }, (err, res) => {
                 if (err) throw err;
-                if (res) return sock.sendMessage(id, { text: "אופס... סטיקר זה כבר קיים במאגר" });
+                if (res) return sendCustomMsgQueue(id, { text: "אופס... סטיקר זה כבר קיים במאגר" });
 
                 // save sticker to database
                 barkuniDB.create({ buffer: stickerBuffer }, (err, res) => {
                     if (err) throw err;
                     console.log(res);
-                    sock.sendMessage(id, { text: "הסטיקר נוסף בהצלחה!" });
+                    sendCustomMsgQueue(id, { text: "הסטיקר נוסף בהצלחה!" });
                 });
             });
         } catch (error) {
             console.error(error);
+            errorMsgQueue("BarkuniSticker" + error);
         }
         return;
     }
 
     // send random sticker from database
-    searchBarkuni(sock, id);
+    searchBarkuni(id);
 }
 
 /**
  * send random sticker from database
- * @param {import('@adiwajshing/baileys').WASocket} sock 
  * @param {string} id 
  */
-async function searchBarkuni(sock, id) {
+async function searchBarkuni(id) {
     const numItems = await barkuniDB.estimatedDocumentCount()
-    if (numItems === 0) return sock.sendMessage(id, { text: "אופס... אין לי סטיקרים" });
+    if (numItems === 0) return sendCustomMsgQueue(id, { text: "אופס... אין לי סטיקרים" });
 
     const rand = Math.floor(Math.random() * numItems)
     const randomItem = await barkuniDB.findOne().skip(rand)
 
     const sticker = new Sticker(randomItem.buffer, {
         author: 'BabiBot',
-        quality: 40
+        pack: 'ברקוני',
+        quality: 30
     });
 
-    sock.sendMessage(id, await sticker.toMessage()).then(messageRetryHandler.addMessage);;
+    sendCustomMsgQueue(id, await sticker.toMessage());
 }
 
 export default BarkuniSticker;
