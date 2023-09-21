7c5fb2e07db448e86e5018d65796d12a63503a37
Shilo Babila <shilobabila1@gmail.com>
2023-09-21 14:35:54 +0300

    Update QueueObj.js

--- src/QueueObj.js
+++ src/QueueObj.js
@@ -57,10 +57,13 @@ export function sendCustomMsgQueue(jid, content, options = {}) {
  * @param {string} jid
  * @param {string} text
  */
-export function errorMsgQueue(text) {
+export async function errorMsgQueue(text) {
     const botNum = GLOBAL.sock.user?.id?.split("@")[0].split(":")[0] + "@s.whatsapp.net";
     const superuserNum = process.env.SUPERUSER + "@s.whatsapp.net";
-    return msgQueue.add(async () => await GLOBAL.sock.sendMessage(superuserNum, { text })
-        .then(messageRetryHandler.addMessage))
-        .catch(() => { console.error("errorMsgQueue: failed to send error message to superuser") });
+    try {
+        return await msgQueue.add(async () => await GLOBAL.sock.sendMessage(superuserNum, { text })
+            .then(messageRetryHandler.addMessage));
+    } catch {
+        console.error("errorMsgQueue: failed to send error message to superuser");
+    }
 }
