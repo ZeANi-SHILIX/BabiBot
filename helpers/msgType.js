7eab0baaf18a33e5346eb3a4bb7df3629ed81c1b
Shilo Babila <shilobabila1@gmail.com>
2024-06-25 19:12:50 +0300

    More types

--- helpers/msgType.js
+++ helpers/msgType.js
@@ -4,7 +4,9 @@ export const MsgType = {
     VIDEO: "video",
     AUDIO: "audio",
     STICKER: "sticker",
-    DOCUMENT: "document"
+    DOCUMENT: "document",
+    REACT: "reaction",
+    OTHER: "other"
 };
 
 /**
@@ -13,10 +15,14 @@ export const MsgType = {
  * @returns default {type: MsgType.TEXT, mime: undefined}
  */
 export function getMsgType(msg) {
-    if (msg?.message?.imageMessage) return {type: MsgType.IMAGE, mime: msg.message.imageMessage.mimetype};
-    if (msg?.message?.videoMessage) return {type: MsgType.VIDEO, mime: msg.message.videoMessage.mimetype};
-    if (msg?.message?.audioMessage) return {type: MsgType.AUDIO, mime: msg.message.audioMessage.mimetype};
-    if (msg?.message?.stickerMessage) return {type: MsgType.STICKER, mime: msg.message.stickerMessage.mimetype}
-    if (msg?.message?.documentMessage) return {type: MsgType.DOCUMENT, mime: msg.message.documentMessage.mimetype}
-    return {type: MsgType.TEXT, mime: undefined};
+    if (msg?.message?.imageMessage) return { type: MsgType.IMAGE, mime: msg.message.imageMessage.mimetype };
+    if (msg?.message?.videoMessage) return { type: MsgType.VIDEO, mime: msg.message.videoMessage.mimetype };
+    if (msg?.message?.audioMessage) return { type: MsgType.AUDIO, mime: msg.message.audioMessage.mimetype };
+    if (msg?.message?.stickerMessage) return { type: MsgType.STICKER, mime: msg.message.stickerMessage.mimetype };
+    if (msg?.message?.documentMessage) return { type: MsgType.DOCUMENT, mime: msg.message.documentMessage.mimetype };
+    if (msg?.message?.reactionMessage) return { type: MsgType.REACT, mime: undefined };
+    if (msg?.message?.extendedTextMessage ||
+        msg?.message?.conversation) return { type: MsgType.TEXT, mime: undefined };
+        
+    return { type: MsgType.OTHER, mime: undefined };
 }
